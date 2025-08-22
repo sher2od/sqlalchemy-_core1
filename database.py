675d0c6bbac41d93ce499a9f026a8225e5f92a0f
sher2od <sherzodqarshiboyev543@gmail.com>
2025-08-22 17:21:12 +0300

    database

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import(
+    create_engine,URL,MetaData
+)
+
+import config
+
+DATABASE_URL = URL.create(
+    "postgresql+psycopg2",
+    username=config.DB_USER,
+    password=config.DB_PASSWORD,
+    host=config.DB_HOST,
+    port=config.DB_PORT,
+    database=config.DB_NAME,
+)
+
+engine = create_engine(DATABASE_URL)
+
+metadata_odj = MetaData()
